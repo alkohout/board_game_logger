1b39b43caa8a8d8e191d93ff170c170ebb343221
alkohout <kohoutal@gmail.com>
2025-07-29 10:04:39 +1200

    update git

--- create.sh
+++
@@ -1,9 +0,0 @@
-# Create a new database called "boardgames"
-createdb boardgames
-
-# Connect to the database and create a table called "games"
-psql -d boardgames -c "CREATE TABLE games (
-    id SERIAL PRIMARY KEY,
-    date_played DATE NOT NULL,
-    game_title VARCHAR(255) NOT NULL
-);"
